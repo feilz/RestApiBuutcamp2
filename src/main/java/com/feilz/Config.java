56f83359369e1a8e13cd79b461cdf559bb0d3130
feilz <ackersdan@gmail.com>
2018-11-20 11:42:07 +0200

    added datasource bean

--- src/main/java/com/feilz/Config.java
+++ src/main/java/com/feilz/Config.java
@@ -1,9 +1,21 @@
 package com.feilz;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 public class Config {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
